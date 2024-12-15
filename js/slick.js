24b012be794f182e29ddfab3bb89cb4c3e6f9619
olesyamaniuk <maniukolesya222@gmail.com>
2024-12-15 17:35:16 +0200

    2

--- js/slick.js
+++ js/slick.js
@@ -16,41 +16,8 @@ $(function () {
 });
 
 
-$(function () {
-  $('.play-list').slick({
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    variableWidth: true,
-    autoplay: true,
-    autoplaySpeed: 3500,
-    speed: 1000,
-    dots: false,
-    centerMode: true,
-    cssEase: 'linear',
-    prevArrow: '.play__list-left',
-    nextArrow: '.play__list-right',
-    
-  });
-});
 
-$(function () {
-  $('.features-list').slick({
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    variableWidth: true,
-    autoplay: true,
-    autoplaySpeed: 3500,
-    speed: 1000,
-    dots: false,
-    centerMode: true,
-    cssEase: 'linear',
-    prevArrow: '.features__list-left',
-    nextArrow: '.features__list-right',
-    
-  });
-});
+
 
 
 
